d3d3de07ec845a0773e05a3594d88e1b64c6681c
hermionearcher <46831152+hermionearcher@users.noreply.github.com>
2024-07-15 18:35:16 +0100

    initial build

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Arkan Football</h1>
+      <div className="card">
+        <p>
+          We're renovating our webiste, check back here soon. In the mean time
+          hers our contact info:
+        </p>
+        <p className="read-the-docs">
+          <a href="mailto:akeem@arkanfootball.co.uk?subject=Hey%2C%20came%20from%20your%20website%20%3A)">
+            akeem@arkanfootball.co.uk
+          </a>{' '}
+        </p>
+        {/* <button onClick={}>
+          count is
+        </button> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
